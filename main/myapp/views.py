f7ad6b25d78e893684f3728db6dd37883850c82d
XieXinCen <10646033@ntub.edu.tw>
2019-06-30 19:04:06 +0800

    Finish ch2-2

--- main/myapp/views.py
+++ main/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}：".format(str(count)) + str(post) + "<hr>")
+        post_lists.append("<small>" + str(post.body + "</small><br><br>"))
+    return HttpResponse(post_lists)
