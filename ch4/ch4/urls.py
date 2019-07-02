c12d54c61e1fb6c2b8764d195b63d5f30f55bb90
XieXinCen <10646033@ntub.edu.tw>
2019-07-02 09:27:34 +0800

    Finish 4.3.3 URL parameter processing

--- ch4/ch4/urls.py
+++ ch4/ch4/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import about, listing
+from mysite.views import about, listing, disp_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about),
     path('list/', listing),
+    path('list/<str:sku>', disp_detail),
 ]
--- ch4/mysite/views.py
+++ ch4/mysite/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Product
 
 # Create your views here.
 
 def about(request):
+    html='''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>歡迎來到我的 ch4 痛苦王國</title>
+    </head>
+    <body>
+        <h1>痛苦痛苦王國</h1>
+        <hr>
+        這裡很痛苦
+    </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
+
+def listing(request):
     html = '''
     <!DOCTYPE html>
     <html lang="en">
@@ -24,7 +44,7 @@ def about(request):
     </html>
     '''
     product = Product.objects.all()
-    tags = '<tr><td>品名</td><td>售價</td><td>庫存量</td></tr>'
+    tags = '<tr><td>品名</td><td>售價</td><td>大小</td></tr>'
     for p in product:
         tags = tags+ '<tr><td>{}</td>'.format(p.name)
         tags = tags+ '<td>{}</td>'.format(p.price)
@@ -33,5 +53,31 @@ def about(request):
     return HttpResponse(html.format(tags))
 
 
-def listing(request):
-    pass
+def disp_detail(request, sku):
+    html='''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{0}</title>
+    </head>
+    <body>
+        <h2>{1}</h2>
+        <hr>
+        <table width=400 border=1 bgcolor=#ccffcc>{2}</table>
+        <a href="/list">回列表</a>
+    </body>
+    </html>
+    '''
+    try:
+        p = Product.objects.get(sku=sku)
+    except Product.DoesNotExist:
+        raise Http404('找不到指定的品項編號')
+    tags = '<tr><td>品項編號</td><td>{}</td></tr>'.format(p.sku)
+    tags = tags + '<tr><td>品項名稱</td><td>{}</td></tr>'.format(p.name)
+    tags = tags + '<tr><td>二手售價</td><td>{}</td></tr>'.format(p.price)
+    tags = tags + '<tr><td>尺寸大小</td><td>{}</td></tr>'.format(p.size)
+
+    return HttpResponse(html.format(p.name, p.name, tags))
