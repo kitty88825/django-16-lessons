e4796e4f32539e2f844d719e17cfcab08f83a6b8
XieXinCen <10646033@ntub.edu.tw>
2019-07-08 21:32:01 +0800

    Finish 7.2.2 use template

--- ch7/mysite/views.py
+++ ch7/mysite/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from mysite import models
 
 # Create your views here.
+def index(request):
+    products = models.Product.objects.all()
+    return render(request, 'index.html', locals())
