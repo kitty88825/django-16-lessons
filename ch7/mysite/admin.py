2858e50ee540393770eec085bcde101ea5a18aa0
XieXinCen <10646033@ntub.edu.tw>
2019-07-08 18:28:21 +0800

    Finish 7.2.1 admin.site.register

--- ch7/mysite/admin.py
+++ ch7/mysite/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+# from mysite.models import Maker, PModel, Product, PPhoto
+from mysite import models
 
 # Register your models here.
+# 另一種import方法
+# admin.site.register(Maker)
+# admin.site.register(PModel)
+# admin.site.register(Product)
+# admin.site.register(PPhoto)
+
+admin.site.register(models.Maker)
+admin.site.register(models.PModel)
+admin.site.register(models.Product)
+admin.site.register(models.PPhoto)
