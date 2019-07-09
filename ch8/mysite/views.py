2de7f6301c7b4593b92a694267d124106f249317
XieXinCen <10646033@ntub.edu.tw>
2019-07-09 17:43:17 +0800

    Finish 8.1.1 add form

--- ch8/mysite/views.py
+++ ch8/mysite/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from mysite import models
 
 # Create your views here.
+def index(request):
+    try:
+        urid = request.GET['user_id']
+        urpass = request.GET['user_pass']
+    except:
+        urid = None # 不要學書上打null
+
+    if urid != None and urpass == '12345':
+        verified = True
+    else:
+        verified = False
+    return render(request, 'index.html', locals())
