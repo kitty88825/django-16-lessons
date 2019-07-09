6e7ccc1c13f5e53332c2bc069d77ba19b837569a
XieXinCen <10646033@ntub.edu.tw>
2019-07-09 20:51:54 +0800

    Finish 8.1.2 try form tag

--- ch8/mysite/views.py
+++ ch8/mysite/views.py
@@ -3,11 +3,14 @@ from mysite import models
 
 # Create your views here.
 def index(request):
+    years = range(1990, 2020)
     try:
         urid = request.GET['user_id']
         urpass = request.GET['user_pass']
+        uryear = request.GET['byear']
+        urfcolor = request.GET.getlist('fcolor')
     except:
-        urid = None # 不要學書上打null
+        urid = None 
 
     if urid != None and urpass == '12345':
         verified = True
