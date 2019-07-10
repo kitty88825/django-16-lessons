39a2de7b4f314f84bae7837f67a360c26b05c7b1
XieXinCen <10646033@ntub.edu.tw>
2019-07-10 10:56:47 +0800

    Finish 8.1.4 show message

--- ch8/mysite/admin.py
+++ ch8/mysite/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from mysite import models
 
 # Register your models here.
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('pmodel', 'nickname', 'price', 'year')
-    search_fields = ('nickname',)
-    ordering = ('-price',)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'message', 'enabled', 'pub_time')
+    ordering = ('-pub_time',)
 
-admin.site.register(models.Maker)
-admin.site.register(models.PModel)
-admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.PPhoto)
+
+admin.site.register(models.Mood)
+admin.site.register(models.Post, PostAdmin)
--- ch8/mysite/views.py
+++ ch8/mysite/views.py
@@ -3,17 +3,7 @@ from mysite import models
 
 # Create your views here.
 def index(request):
-    years = range(1990, 2020)
-    try:
-        urid = request.GET['user_id']
-        urpass = request.GET['user_pass']
-        uryear = request.GET['byear']
-        urfcolor = request.GET.getlist('fcolor')
-    except:
-        urid = None 
+    posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
+    moods = models.Mood.objects.all()
 
-    if urid != None and urpass == '12345':
-        verified = True
-    else:
-        verified = False
     return render(request, 'index.html', locals())
