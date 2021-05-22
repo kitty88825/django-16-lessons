e6b2fb276eeb99517e43ce77fee9afa2788d1918
kitty88825 <kitty40632@gmail.com>
2021-05-22 16:30:17 +0800

    4.2.2 在 admin.py 中建立資料表管理介面

--- mynewsite/mysite/models.py
+++ mynewsite/mysite/models.py
@@ -12,3 +12,16 @@ class NewTable(models.Model):
     float_f = models.FloatField(null=True)
     int_f = models.IntegerField(default=2010)
     text_f = models.TextField()
+
+
+
+class Product(models.Model):
+    SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    sku = models.CharField(max_length=5)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1, choices=SIZES)
