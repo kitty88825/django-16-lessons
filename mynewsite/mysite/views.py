a485ce25f3133e1372247c0054d8b1a769add631
kitty88825 <kitty40632@gmail.com>
2021-05-22 17:35:54 +0800

    4.4.2 傳遞變數到 template 檔案中

--- mynewsite/mysite/views.py
+++ mynewsite/mysite/views.py
@@ -1,29 +1,22 @@
+import random
+
 from django.http import HttpResponse
 from django.http.response import Http404
+from django.shortcuts import render
 
 from .models import Product
 
 
 def about(request):
-    html = '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>About Myself</title>
-    </head>
-    <body>
-        <h2>Xin-Cen Xie</h2>
-        <hr>
+    quotes = [
+        '今日事，今日畢',
+        '要怎麼收穫，先那麼栽',
+        '知識就是力量',
+        '一個人的個性就是他的命運',
+    ]
+    quotes = random.choice(quotes)
 
-        <p>Hi, I am Xin-Cen Xie. Nice to meet you!</p>
-    </body>
-    </html>
-    '''
-
-    return HttpResponse(html)
+    return render(request, 'about.html', locals())
 
 
 def listing(request):
@@ -57,33 +50,9 @@ def listing(request):
 
 
 def disp_detail(request, sku):
-    html = '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{}</title>
-    </head>
-    <body>
-        <h2>{}</h2>
-        <hr>
-        <table width=400 border=1 bgcolor="#CCFFCC">
-        {}
-        </table>
-        <a href="/list">回列表</a>
-    </body>
-    </html>
-    '''
     try:
         p = Product.objects.get(sku=sku)
     except Product.DoesNotExist:
         raise Http404('找不到指定的品項編號')
 
-    tags = f'<tr><td>品項編號</td><td>{p.sku}</td></tr>'
-    tags = tags + f'<tr><td>品項名稱</td><td>{p.name}</td></tr>'
-    tags = tags + f'<tr><td>二手售價</td><td>{p.price}</td></tr>'
-    tags = tags + f'<tr><td>大小</td><td>{p.size}</td></tr>'
-
-    return HttpResponse(html.format(p.name, p.name, tags))
+    return render(request, 'disp.html', locals())
