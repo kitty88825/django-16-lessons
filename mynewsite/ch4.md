4ac8b19f92339dace76cf8a576e5a418cc05eb35
kitty88825 <kitty40632@gmail.com>
2021-05-22 16:24:56 +0800

    4.2.1 在 modeles.py 中建立資料表

---
+++ mynewsite/ch4.md
@@ -0,0 +1,8 @@
+# Ch4
+查看 migrations 轉換成 SQL 指令
+```
+python manage.py sqlmigrate $APP_NAME $Migrate_file
+```
+```
+python manage.py sqlmigrate mysite 0001
+```
--- mynewsite/mysite/models.py
+++ mynewsite/mysite/models.py
@@ -1,1 +1,14 @@
+from datetime import datetime
 from django.db import models
+
+
+class NewTable(models.Model):
+    bigint_f = models.BigIntegerField()
+    bool_f = models.BooleanField()
+    date_f = models.DateField(auto_now=True)
+    char_f = models.CharField(max_length=20, unique=True)
+    datetime_f = models.DateTimeField(auto_now_add=True)
+    decimal_f = models.DecimalField(max_digits=10, decimal_places=2)
+    float_f = models.FloatField(null=True)
+    int_f = models.IntegerField(default=2010)
+    text_f = models.TextField()
