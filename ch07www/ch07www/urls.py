20016d1cfa617e1acfce79a6e51d24783e125566
kitty88825 <kitty40632@gmail.com>
2021-05-22 23:37:33 +0800

    7.2.3 多資料表整合詢網頁製作

--- ch07www/ch07www/urls.py
+++ ch07www/ch07www/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mysite.views import detail, index
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('detail/<int:id>', detail, name='detail-url'),
 ]
--- ch07www/mysite/views.py
+++ ch07www/mysite/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import PPhoto, Product
+
+
+def index(request):
+    products = Product.objects.all()
+
+    return render(request, 'index.html', locals())
+
+
+def detail(request, id):
+    try:
+        product = Product.objects.get(id=id)
+        images = PPhoto.objects.filter(product=product)
+    except Product.DoesNotExist:
+        pass
+
+    return render(request, 'detail.html', locals())
