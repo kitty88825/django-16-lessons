1f7dfd1f6f20e461b230d193beddb8a85aeaa331
kitty88825 <kitty40632@gmail.com>
2021-05-22 16:40:27 +0800

    4.3.1 建立簡易的 HttpResponse 網頁

--- mynewsite/mysite/views.py
+++ mynewsite/mysite/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def about(request):
+    html = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About Myself</title>
+    </head>
+    <body>
+        <h2>Xin-Cen Xie</h2>
+        <hr>
+
+        <p>Hi, I am Xin-Cen Xie. Nice to meet you!</p>
+    </body>
+    </html>
+    '''
+
+    return HttpResponse(html)
