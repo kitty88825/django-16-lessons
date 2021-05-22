0f6ec7f0b77ed27c8d2809a5938a647d59cbd310
kitty88825 <kitty40632@gmail.com>
2021-05-22 22:53:36 +0800

    7.2.1 建立網站

--- ch07www/mysite/admin.py
+++ ch07www/mysite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Maker, PModel, PPhoto, Product
+
+
+admin.site.register(Maker)
+admin.site.register(PModel)
+admin.site.register(Product)
+admin.site.register(PPhoto)
