1fb0a8ed65db5cba8c74c6f07464ae78a0098489
kitty88825 <kitty40632@gmail.com>
2021-05-22 17:42:23 +0800

    4.4.3 在 template 中處理串列變數

--- mynewsite/mysite/views.py
+++ mynewsite/mysite/views.py
@@ -1,6 +1,5 @@
 import random
 
-from django.http import HttpResponse
 from django.http.response import Http404
 from django.shortcuts import render
 
@@ -20,33 +19,9 @@ def about(request):
 
 
 def listing(request):
-    html = '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>中古機列表</title>
-    </head>
-    <body>
-        <h2>以下是目前本店販售中的二手機列表</h2>
-        <hr>
-        <table width=400 border=1 bgcolor="#CCFFCC">
-        {}
-        </table>
-    </body>
-    </html>
-    '''
-
     products = Product.objects.all()
-    tags = '<tr><td>品名</td><td>售價</td><td>大小</td></tr>'
-    for p in products:
-        tags = tags + f'<tr><td>{p.name}</td>'
-        tags = tags + f'<td>{p.price}</td>'
-        tags = tags + f'<td>{p.size}</td></tr>'
 
-    return HttpResponse(html.format(tags))
+    return render(request, 'list.html', locals())
 
 
 def disp_detail(request, sku):
