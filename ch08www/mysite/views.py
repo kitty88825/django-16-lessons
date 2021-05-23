91fdbbc19ed51279fead8de7d43032d69da31075
kitty88825 <kitty40632@gmail.com>
2021-05-23 12:24:40 +0800

    8.1.5 接收表單資料儲存於資料庫中

--- ch08www/mysite/views.py
+++ ch08www/mysite/views.py
@@ -6,5 +6,19 @@ from .models import Mood, Post
 def index(request):
     posts = Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
     moods = Mood.objects.all()
+    try:
+        user_id = request.GET['user_id']
+        user_pass = request.GET['user_pass']
+        user_post = request.GET['user_post']
+        user_mood = request.GET['mood']
+    except:
+        user_id = None
+        message = '如要張貼訊息，則每一個欄位都要填...'
+
+    if user_id != None:
+        mood = Mood.objects.get(status=user_mood)
+        post = Post.objects.create(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+        post.save()
+        message = f'成功儲存！請記得你的編輯密碼[{user_pass}]!訊息須經審查後才會顯示。'
 
     return render(request, 'index.html', locals())
