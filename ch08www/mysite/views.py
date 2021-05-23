545b81ec400e9c0039c301778fae88bb41698bb6
kitty88825 <kitty40632@gmail.com>
2021-05-23 11:20:31 +0800

    8.1.1 HTML<form>表單簡介

--- ch08www/mysite/views.py
+++ ch08www/mysite/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    try:
+        urid = request.GET['user_id']
+        urpass = request.GET['user_pass']
+    except:
+        urid = None
+
+    if urid != None and urpass == '12345':
+        verified = True
+    else:
+        verified = False
+
+    return render(request, 'index.html', locals())
